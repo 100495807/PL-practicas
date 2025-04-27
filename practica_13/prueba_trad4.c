069abdb19c092f2526f1007fde05230ef7583a68
100495807 <158489101+100495807@users.noreply.github.com>
2025-04-27 23:04:00 +0200

    funciona todas pruebas jimeno

---
+++ practica_13/prueba_trad4.c
@@ -0,0 +1,43 @@
+#include <stdio.h> 
+
+square (int v) { 
+    return (v*v) ; 
+} 
+ 
+fact (int n) { 
+   int f ; 
+   if (n == 1) { 
+      f = 1 ; 
+   } else { 
+     f = n * fact (n-1) ; 
+   } 
+   return f ; 
+} 
+ 
+is_even (int v) { 
+   int ep  ; 
+   int mivector[10]; 
+   printf ("%d", v) ; 
+   while (v > 0) { 
+      v = v ; 
+   }
+   if (v % 2 == 0) { 
+      puts (" is even") ; 
+      ep = 1 ; 
+   } else { 
+      puts (" is odd") ; 
+      ep = 0 ; 
+   }
+   mivector [5] = 123 ;
+   return ep ; 
+} 
+ 
+main () { 
+   printf ("%d\n", square (7)) ; 
+   printf ("%d\n", fact (7)) ; 
+   printf ("%d\n", is_even (7)) ; 
+   printf ("%d\n", is_even (8)) ; 
+   is_even (8) ;
+}
+
+//@ (main)
